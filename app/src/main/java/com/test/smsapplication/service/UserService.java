df8d65efa660a2fd1ec6ff0da14aa43f2216f2ff
Yorvoration <dev.dilshodjon@gmail.com>
2023-03-03 15:39:57 +0500

    works

--- app/src/main/java/com/test/smsapplication/service/UserService.java
+++ app/src/main/java/com/test/smsapplication/service/UserService.java
@@ -2,12 +2,21 @@ package com.test.smsapplication.service;
 
 import com.test.smsapplication.models.DataClass;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.PUT;
+import retrofit2.http.Query;
 
 public interface UserService {
     @GET("/sms")
     Call<DataClass> getData();
-    @GET("/sms/status?status=2")
-    Call<DataClass> getData1();
+
+    @GET("/sms/status")
+    Call<DataClass> updateStatus(@Query("status") String status);
+
+    //body   [1,2]
+    @PUT("/sms")
+    Call<List<int[]>> updateSmsStatus(List<int[]> body);
 }
